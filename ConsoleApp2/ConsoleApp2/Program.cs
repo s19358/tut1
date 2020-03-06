0caec18ea49464e24edb00194d9313d809af1407
s19358 <s19358@LAB204-12>
2020-03-06 13:46:59 +0100

    task3 started

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    public class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var url = "https://www.pja.edu.pl/";
+            var httpClient = new HttpClient();
+
+
+            var response = await httpClient.GetAsync(url);
+            var regex = new Regex(@"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$");
+            var content = response.Content;
+            var matches = regex.Matches(content.ToString());
+
+            foreach(var match in matches)
+            {
+                Console.WriteLine(match.ToString());
+            }
+
+
+
+           // Console.WriteLine("Hello World!");
+        }
+    }
+}
