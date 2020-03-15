4c8a3b5c1300514003f5a3560cfbb421d7cef020
s19358 <s19358@pjwstk.edu.pl>
2020-03-15 20:09:55 +0100

    task5

---
+++ ConsoleApp2/task3/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using System.Net.Http;
+using System.Reflection;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    public class Program
+    {
+        static async Task Main(string[] args) 
+        {
+          
+             string _filePath = Path.GetDirectoryName(System.AppDomain.CurrentDomain.BaseDirectory);
+            _filePath = Directory.GetParent(_filePath).FullName;
+
+           
+            using (var reader = new StreamReader(_filePath))
+            {
+                var line = reader.ReadLine();
+                Console.WriteLine(line);
+                
+            }
+
+
+        }
+
+        private static object GetResourceStream(string filePath)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
